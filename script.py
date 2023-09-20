71d7530e4f3c420e9f49372cb913160e97af35ab
Nhat Minh Luu <54903938+nluu175@users.noreply.github.com>
2023-09-19 20:51:26 -0600

    Added script to download source code

--- script.py
+++ script.py
@@ -5,3 +5,9 @@ print(requests.__version__)
 response = requests.get("http://google.com/")
 
 print(response)
+
+response_to_script = requests.get(
+    "https://raw.githubusercontent.com/nluu175/cmput404-lab1/main/script.py"
+)
+
+print(response_to_script.content)
